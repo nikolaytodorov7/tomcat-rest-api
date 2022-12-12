82f22c5f2592ecc8d6e57b8640daf2d3d259499c
Nikolay Todorov <105609346+nikolaytodorov7@users.noreply.github.com>
2022-12-12 11:30:05 +0200

    Code cleanup
    AuthenticationFilter code cleaned.

--- webapp/WEB-INF/src/filter/AuthenticationFilter.java
+++ webapp/WEB-INF/src/filter/AuthenticationFilter.java
@@ -23,31 +23,28 @@ public class AuthenticationFilter extends HttpFilter {
     private static String user = null;
     private static final Logger LOGGER = LogManager.getLogger(AuthenticationFilter.class.getName());
 
-
     public void doFilter(HttpServletRequest req, HttpServletResponse resp, FilterChain chain) throws IOException, ServletException {
-        String method = req.getMethod();
-        String servletPath = req.getServletPath();
         String path = buildPath(req);
-        if (session == null && (LOGIN_PATTERN.matcher(path).matches() || REGISTER_PATTERN.matcher(path).matches())) {
+        if (LOGIN_PATTERN.matcher(path).matches() || REGISTER_PATTERN.matcher(path).matches()) {
             chain.doFilter(req, resp);
             session = req.getSession();
             User sessionUser = (User) session.getAttribute("user");
             user = sessionUser.username;
-            String logMessage = String.format("%s %s %s", user, method, path);
+            String logMessage = String.format("%s %s", user, req.getServletPath());
             LOGGER.info(logMessage);
             return;
         }
 
         if (session == null) {
+            String logMessage = String.format("%s %s", user, path);
+            LOGGER.error(logMessage);
             StatusMessage msg = new StatusMessage(401, "There is no active session, please log in.");
             writeAsJson(resp, msg);
-            String logMessage = String.format("%s %s %s", user, method, path);
-            LOGGER.info(logMessage);
             return;
         }
 
-        chain.doFilter(req, resp);
-        String logMessage = String.format("%s %s %s", user, method, path);
+        String logMessage = String.format("%s %s", user, path);
         LOGGER.info(logMessage);
+        chain.doFilter(req, resp);
     }
 }
